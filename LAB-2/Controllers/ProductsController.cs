e2669e7aecf92d4a631199160e7f6315ea65cef6
badunmo <barry.adunmo@MyGeorgian.ca>
2018-09-24 13:42:12 -0400

    second commit

---
+++ LAB-2/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LAB_2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: Product/Details
+        public ActionResult Details(string ProductName)
+        {
+            ViewBag.ProductName = ProductName;
+            return View();
+        }
+    }
+}
